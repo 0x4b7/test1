31c9ad3eaeb15a90b5a4ab944988c3e5eeba2308
first <it-is-post@yandex.ru>
2023-03-06 23:25:49 +0400

    init lesson2

--- lesson2.py
+++ lesson2.py
@@ -1,6 +1,14 @@
+def custom_filter(some_list):
+    res = [i for i in some_list if isinstance(i, int)]
+    s = sum([j for j in res if j % 7 == 0])
+    return s < 83
+
 
 def main():
-    pass
+    some_list = [7, 14, 28, 32, 32, 56]
+
+    print(custom_filter(some_list))
+
 
 if __name__ == '__main__':
     main()
