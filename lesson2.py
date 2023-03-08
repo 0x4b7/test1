0a2c555fc50bf23df80e1f67db28eb029160f6b6
first <it-is-post@yandex.ru>
2023-03-08 14:41:27 +0400

    Удачная рекурсия с круглыми скобками добавил вложенность

--- lesson2.py
+++ lesson2.py
@@ -1,29 +1,33 @@
-def r(s, ch):
-    if len(s) == 0:
+def r(s, ch, vlg):
+    if len(s) == 0 and vlg==0:
         return True
+    elif len(s)==0 and vlg!=0:
+        return False
     else:
         if s[0] == '(':
+            vlg+=1
             ch = s[0]
             s = s[1:]
-            if r(s, ch):return True
+            if r(s, ch, vlg):return True
             else:
                 return False
         elif s[0] == ')' and ch == '':
             return False
         elif s[0] == ')' and ch == '(':
-            ch = ''
+            vlg-=1
+            if vlg==0:ch = ''
             s = s[1:]
-            if r(s, ch):
+            if r(s, ch, vlg):
                 return True
             else:
                 return False
         else:
             s = s[1:]
-            if r(s, ch):
+            if r(s, ch, vlg):
                 return True
             else:
                 return False
 
 
 
-print(r('a(c)d()(d)', ''))
+print(r('a(())(c)d(()(()))(d)', '', 0))
