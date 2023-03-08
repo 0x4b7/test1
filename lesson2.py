73c2677fb677a9dd28e846ff1f79404269c84c00
first <it-is-post@yandex.ru>
2023-03-08 17:10:30 +0400

    Рекурсия для проверки правильной скобочной записи тре видов скобки(){}[]

--- lesson2.py
+++ lesson2.py
@@ -1,33 +1,39 @@
-def r(s, ch, vlg):
-    if len(s) == 0 and vlg==0:
-        return True
-    elif len(s)==0 and vlg!=0:
+def r(s, ch, vlg=[]):
+
+# Чтобы не писать кучу условий, сделал словарь, здесь тип откр собки соответствуе  типу закрытой
+# скобки sootvet[ '(' ] = ')'
+    sootvet = {'(': ')', '[': ']', '{': '}', '': ''}
+
+
+    if len(s) == 0 and len(vlg) == 0:                #
+        return True                                  #Условие окончания рекурсии, если строка кончилась
+    elif len(s) == 0 and len(vlg) != 0:              # и стек пустой
         return False
+
+# Иначе если строка не кончилась ищем открытые скобки
     else:
-        if s[0] == '(':
-            vlg+=1
+        if (s[0] == '(') or (s[0] == '[') or (s[0] == '{'):
+            vlg.append(s[0])
             ch = s[0]
             s = s[1:]
-            if r(s, ch, vlg):return True
-            else:
-                return False
-        elif s[0] == ')' and ch == '':
+            return r(s, ch, vlg)
+# проверяем если следущий символ закрывающая скобка, а открывающей не было или был другой тип [} (] [)
+        elif ((s[0] == ')') or (s[0] == ']') or (s[0] == '}')) and sootvet[ch] != s[0]:
             return False
-        elif s[0] == ')' and ch == '(':
-            vlg-=1
-            if vlg==0:ch = ''
-            s = s[1:]
-            if r(s, ch, vlg):
-                return True
+# проверяем если следущий символ закрывающая скобка и была открывающая того же типа
+        elif ((s[0] == ')') or (s[0] == ']') or (s[0] == '}')) and sootvet[ch] == s[0]:
+            vlg.pop()
+            if len(vlg) == 0:
+                ch = ''
             else:
-                return False
+                ch=vlg[-1]
+
+            s = s[1:]
+            return r(s, ch, vlg)
+# проверяем если следущий символ не наша скобка или любой символ
         else:
             s = s[1:]
-            if r(s, ch, vlg):
-                return True
-            else:
-                return False
+            return r(s, ch, vlg)
 
 
-
-print(r('a(())(c)d(()(()))(d)', '', 0))
+print(r('()({}[([{[]}])()((()))])[]', '', []))
